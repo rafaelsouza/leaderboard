5f10b4978ec14ced1fad335d6b8f7e22d5cd70e8
Rafael Souza <rcoelhodesouza@gmail.com>
2014-03-25 22:45:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeagueApp::Application.config.secret_key_base = 'fc9933d96abd8e93f4dd8284d643c5ead4cf2e614258f3f4e6f8ed0c59c3125bcd1a1cf078ad944cfd1988afd8bda2e5477b62d001803d85c829c6250981145b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LeagueApp::Application.config.session_store :cookie_store, key: '_league_app_session'
